2a4aa8ae4fff6714c9e4d9b75afc10d8ed25a762
Laurent Valdes <valderama@gmail.com>
2018-05-31 19:53:46 +0200

    Add @todo

--- bidswitch-json/src/test/scala/com/powerspace/openrtb/bidswitch/BidSwitchSerDeTest.scala
+++ bidswitch-json/src/test/scala/com/powerspace/openrtb/bidswitch/BidSwitchSerDeTest.scala
@@ -10,7 +10,7 @@ import org.scalatest.{FunSuite, GivenWhenThen}
 class BidSwitchSerDeTest extends FunSuite with GivenWhenThen {
   import BidSwitchSerdeModule._
 
-  test("bidswitch parsing") {
+  test("bidswitch response parsing") {
     val stream: URL = getClass.getResource("/bidswitch-bidresponse.json")
 
     Given("A bidswitch bidresponse")
@@ -20,12 +20,15 @@ class BidSwitchSerDeTest extends FunSuite with GivenWhenThen {
     Then("it should return a proper BidResponse")
     val decoded = decode[BidResponse](json)
 
+    // @todo provide implicit accessors on BidSwitch structures instead of relying on the extension thing
+    // @todo improve toString with extensions
+
     println(decoded.map(_.toProtoString))
     println(decoded.map(_.extension(BidswitchProto.bidResponse).map(_.toProtoString)))
     println(decoded.map(_.seatbid.flatMap(_.bid).map(_.extension(BidswitchProto.bidExt).map(_.toProtoString))))
   }
 
-  test("bidswitch deser") {
+  test("bidswitch response serialization") {
     Given("A bidrequest")
     ???
 
@@ -35,4 +38,28 @@ class BidSwitchSerDeTest extends FunSuite with GivenWhenThen {
     Then("it should return a proper JSON")
     ???
   }
+
+  test("bidswitch request parsing") {
+    Given("A bidrequest")
+    ???
+
+    When("I serialize it")
+    ???
+
+    Then("it should return a proper JSON")
+    ???
+  }
+
+  test("bidswitch request serialization") {
+    Given("A bidrequest")
+    ???
+
+    When("I serialize it")
+    ???
+
+    Then("it should return a proper JSON")
+    ???
+  }
+
+
 }
